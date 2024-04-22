09871b524bb530b870b063e1f2e312853fee8807
asfalfa <glkin9@gmail.com>
2024-04-22 17:08:12 +0200

    feat(front): :bricks: categorized components

--- components/Navmenu/Navmenu.vue
+++ components/Navmenu/Navmenu.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import { useUser } from '~/composables/states'
 import { logoutUser } from '~/utils/users'
+import ThemeToggle from './ThemeToggle.vue';
+import RegisterForm from './RegisterForm.vue';
+import LoginForm from './LoginForm.vue';
 const user: Ref<JWTUser> = useUser()
 
 const openRegister = ref<boolean>(false)
 const openLogin = ref<boolean>(false)
 </script>
 <template>
-    <UPopover>
-        <UButton color="white" label="Open" />
+    <UPopover class="absolute top-20 right-[10rem]" mode="hover">
+        <UButton color="white" variant="outline" size="lg" icon="i-heroicons-queue-list-solid" />
         <template #panel>
             <UCard>
                 <div class="grid grid-cols-2 gap-2">
@@ -25,11 +28,21 @@ const openLogin = ref<boolean>(false)
                         <UButton variant="outline" icon="i-heroicons-magnifying-glass"  />
                     </UTooltip>
                 </div>
-                <UDivider icon="i-heroicons-user-circle" class="py-4" />
+                <UDivider class="my-2" />
                 <div class="flex items-center">
                     <div v-if="user" class="w-full flex flex-col gap-2 items-center">
-                        <span class="uppercase font-bold m-auto">{{user.name}}</span>
-                        <UButton label="Log Out" class="bg-red-500 dark:bg-red-600 hover:bg-red-600 hover:dark:bg-red-700 uppercase font-bold dark:text-secondary" @click="logoutUser()" />
+                        <span class="uppercase font-semibold m-auto">{{user.name}}</span>
+                        <div class="grid grid-cols-2 gap-2">
+                            <UTooltip text="Settings">
+                                <UButton color="blue" variant="outline" icon="i-heroicons-cog-6-tooth-solid" />
+                            </UTooltip>
+                            <UTooltip text="Logout">
+                                <UButton color="red" variant="outline" icon="i-heroicons-arrow-right-on-rectangle-solid" @click="logoutUser()" />
+                            </UTooltip>
+                            <UTooltip v-if="user.role == 'USER'" text="Admin">
+                                <UButton color="yellow" variant="outline" icon="i-heroicons-lock-closed-solid" />
+                            </UTooltip>
+                        </div>
                     </div>
                     <div v-else class="w-full flex flex-col gap-2 items-center">
                         <UButton label="Login" class="uppercase font-bold dark:text-secondary" @click="openLogin = true" />
