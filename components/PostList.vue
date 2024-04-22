fe282dbb429f8832e46cb5a51870c39877d31ecf
asfalfa <glkin9@gmail.com>
2024-04-22 19:45:53 +0200

    feat(front): :sparkles: Add NuxtLink to Post in PostList

--- components/PostList.vue
+++ components/PostList.vue
@@ -30,39 +30,43 @@ onMounted(() => {
 </script>
 
 <template>
-    <UContainer class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <!-- grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 -->
+    <UContainer class="grid grid-flow-col auto-cols-max gap-4">
+        
         <UCard 
         class="relative"
         v-for="post in posts" 
         :key=post.title>
-            <div 
-            v-if="post.covers && post.covers[0]" 
-            className='rounded-t-md overflow-hidden grid content-center'>
-                <img className='rounded-t-md h-[150px] md:h-[200px] align-center w-full' :src=post.covers[0] />
-            </div>
-            <div className='mx-4 md:mx-6 absolute left-0 right-0 top-[100px] bottom-[100px] md:top-[130px] md:bottom-[130px] lg:top-[160px] lg:bottom-[160px] z-10 bg-neutral-800/80 p-2 items-center flex flex-col justify-center'>
-                <div class="flex flex-col justify-center text-center">
-                    <Date :date="post.date" />
-                    <div class="flex">
-                        <div 
-                        class="mx-[2px] px-[8px]" 
-                        v-for="cat in post.categories.slice(0, 4)">
-                        {{ cat }}
+            <NuxtLink :to="`/archive/${post.id}`">
+                <div 
+                v-if="post.covers && post.covers[0]" 
+                className='rounded-t-md overflow-hidden grid content-center'>
+                    <img className='rounded-t-md h-[150px] md:h-[200px] align-center w-full' :src=post.covers[0] />
+                </div>
+                <div className='mx-4 md:mx-6 absolute left-0 right-0 top-[100px] bottom-[100px] md:top-[130px] md:bottom-[130px] lg:top-[160px] lg:bottom-[160px] z-10 bg-neutral-800/80 p-2 items-center flex flex-col justify-center'>
+                    <div class="flex flex-col justify-center text-center">
+                        <Date :date="post.date" />
+                        <div class="flex">
+                            <div 
+                            class="mx-[2px] px-[8px]" 
+                            v-for="cat in post.categories.slice(0, 4)">
+                            {{ cat }}
+                            </div>
                         </div>
                     </div>
+                    <div className='font-semibold text-xl'>{{post.title}}</div>
+                    <div>
+                        <small>
+                            by {{post.author.name}}
+                        </small>
+                    </div>
                 </div>
-                <div className='font-semibold text-xl'>{{post.title}}</div>
-                <div>
-                    <small>
-                        by {{post.author.name}}
-                    </small>
+                <div 
+                v-if="post.covers && post.covers[1]" 
+                className='rounded-b-md overflow-hidden grid content-center'>
+                    <img className='rounded-b-md h-[150px] md:h-[200px] align-center w-full' :src=post.covers[1] />
                 </div>
-            </div>
-            <div 
-            v-if="post.covers && post.covers[1]" 
-            className='rounded-b-md overflow-hidden grid content-center'>
-                <img className='rounded-b-md h-[150px] md:h-[200px] align-center w-full' :src=post.covers[1] />
-            </div>
+            </NuxtLink>
         </UCard>
     </UContainer>
 </template>
