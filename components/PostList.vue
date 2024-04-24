8fbec33f5ed1f82b6efdbc8e8de5bb1e1eddafcb
asfalfa <glkin9@gmail.com>
2024-04-24 19:11:11 +0200

    feat(front): :sparkles: Turned card posts into its own component

--- components/PostList.vue
+++ components/PostList.vue
@@ -35,40 +35,9 @@ onMounted(() => {
 <template>
     <!-- grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 -->
     <UContainer class="grid grid-flow-col auto-cols-max gap-4">
-        <UCard 
-        class="relative hover:contrast-[1.1] transition duration-200"
+        <PostCard
         v-for="post in posts" 
-        :key=post.title>
-            <NuxtLink :to="`/archive/${post.id}`">
-                <div 
-                v-if="post.covers && post.covers[0]" 
-                className='rounded-t-md overflow-hidden grid content-center'>
-                    <img className='rounded-t-md h-[150px] md:h-[200px] align-center w-full' :src=post.covers[0] />
-                </div>
-                <div className='mx-4 md:mx-6 absolute left-0 right-0 top-[100px] bottom-[100px] md:top-[130px] md:bottom-[130px] lg:top-[160px] lg:bottom-[160px] z-10 bg-neutral-100/85 dark:bg-neutral-900/85 p-2 items-center flex flex-col justify-center'>
-                    <div class="text-sm flex flex-col justify-center text-center">
-                        <Date :date="post.date" />
-                        <div class="flex">
-                            <div 
-                            class="mx-[2px] px-[8px]" 
-                            v-for="cat in post.categories.slice(0, 4)">
-                            {{ cat }}
-                            </div>
-                        </div>
-                    </div>
-                    <div className='font-semibold text-2xl'>{{post.title}}</div>
-                    <div>
-                        <div class="text-sm">
-                            by {{post.author.name}}
-                        </div>
-                    </div>
-                </div>
-                <div 
-                v-if="post.covers && post.covers[1]" 
-                className='rounded-b-md overflow-hidden grid content-center'>
-                    <img className='rounded-b-md h-[150px] md:h-[200px] align-center w-full' :src=post.covers[1] />
-                </div>
-            </NuxtLink>
-        </UCard>
+        :post="post"
+        :key=post.title />
     </UContainer>
 </template>
