0317b72f0cb868592174ed68bbe601f280efc2c6
asfalfa <glkin9@gmail.com>
2024-04-24 19:10:44 +0200

    chore(front): :bug: rename bg image for clarity

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import bgDark from '@/public/bg-dark.jpg'
+import bgDesktop from '@/public/bg-desktop.jpg'
 import bgMobile from '@/public/bg-mobile.jpg'
 import { getUser } from '~/utils/users'
 const user: Ref<JWTUser> = useUser()
@@ -12,8 +12,8 @@ if(!user.value && token.value){
 <template>
     <!-- transition ease-in-out duration-1000 grayscale-0 lg:grayscale hover:grayscale-0  -->
     <main 
-    :style="{ backgroundImage: `url(${isMobileOrTablet ? bgMobile : bgDark})` }" 
-    class="animated-background min-h-screen flex min-w-screen overflow-x-hidden bg-repeat-round">  
+    :style="{ backgroundImage: `url(${isMobileOrTablet ? bgMobile : bgDesktop})` }" 
+    class="animated-background min-h-screen flex min-w-screen overflow-x-hidden bg-cover">  
         <NuxtLayout name="default">
             <div 
             class="min-w-full min-h-screen items-center flex bg-gradient-to-br animated-background from-orange-400/50 via-cyan-400/50 to-emerald-600/50 dark:from-orange-800/50 dark:via-neutral-700/50 dark:to-neutral-900/50" >
