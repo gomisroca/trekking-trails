3a01ab2352bf34a4b7608158d62c54f56e452a5e
asfalfa <glkin9@gmail.com>
2024-04-25 21:04:58 +0200

    chore(front): :fire: moved admin routes around

---
+++ pages/admin/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <UContainer>
+        <UCard>
+            <div class="py-4 flex justify-center uppercase font-semibold">
+                <span>Admin Panel</span>
+            </div>
+            <div class="flex flex-col gap-4">
+                <UCard>
+                    <div class="flex gap-4">
+                        <UButton variant="outline" label="See Posts" to="/admin/posts/" />
+                        <UButton variant="outline" label="Add Post" to="/admin/posts/upload" />
+                    </div>
+                </UCard>
+                <UCard>
+                    <div class="flex gap-4">
+                        <UButton variant="outline" label="See Users" to="/admin/users/" />
+                    </div>
+                </UCard>
+            </div>
+        </UCard>
+    </UContainer>
+</template>
